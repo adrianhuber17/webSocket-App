de44b5133f8032944e23562679c6ba95ebb894a8
adrianhuber17 <adrianhuber17@gmail.com>
2022-06-01 11:19:21 -0700

    fixed updated server and app

--- server.py
+++ server.py
@@ -9,23 +9,26 @@ socketio = SocketIO(app,cors_allowed_origins="*")
 
 @app.route("/http-call")
 def http_call():
+    """return JSON with string data as the value"""
     data = {'data':'This text was fetched using an HTTP call to server on render'}
     return jsonify(data)
 
 @socketio.on("connect")
 def connected():
+    """event listener when client connects to the server"""
     print(request.sid)
     print("client has connected")
     emit("connect",{"data":f"id: {request.sid} is connected"})
 
 @socketio.on('data')
 def handle_message(data):
+    """event listener when client types a message"""
     print("data from the front end: ",str(data))
     emit("data",{'data':data,'id':request.sid},broadcast=True)
 
 @socketio.on("disconnect")
 def disconnected():
-
+    """event listener when client disconnects to the server"""
     print("user disconnected")
     emit("disconnect","user disconnected",broadcast=True)
 
