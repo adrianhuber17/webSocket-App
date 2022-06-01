377b057bfb2e94550ed347c737f87e937ddb9441
adrianhuber17 <adrianhuber17@gmail.com>
2022-06-01 14:44:32 -0700

    added comments to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,19 +8,28 @@ function App() {
   const [socketInstance, setSocketInstance] = useState("");
   const [loading, setLoading] = useState(true);
 
+  // Use effect to be called on render of page only
   useEffect(() => {
+    //Create socket.io connection on rendering of page
     const socket = io("localhost:5001/", {
       transports: ["websocket"],
       cors: {
         origin: "http://localhost:3000/",
       },
     });
+
+    //set the socket connection instance to the useState
     setSocketInstance(socket);
+
+    //send message to server that user has connected
     socket.on("connect", (data) => {
       console.log(data);
     });
+
     setLoading(false);
   }, []);
+
+  //send message to server and all other clients that a user has disconnected
   if (socketInstance !== "") {
     socketInstance.on("disconnect", (data) => {
       console.log(data);
